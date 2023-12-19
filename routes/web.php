5e32e109d8736cc0b71946158cbb26b5385d114a
999joaquin <joaquinwilbert166@gmail.com>
2023-12-20 02:56:56 +0700

    joaquin-commit2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
 Route::resource('patients', 'PatientController');
 Route::resource('rekammedis', 'rekammedisController');
+Route::delete('/patients/{id}', 'PatientController@destory')->name('patient.destroy');
